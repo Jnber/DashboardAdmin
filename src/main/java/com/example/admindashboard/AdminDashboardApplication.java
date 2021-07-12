34a055da3396f0671b02af05f75c9faa28e34b49
Essia Ben Hamida <benhamidaessia@gmail.com>
2021-07-12 11:39:42 +0100

    first step

---
+++ src/main/java/com/example/admindashboard/AdminDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.admindashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDashboardApplication.class, args);
+    }
+
+}
